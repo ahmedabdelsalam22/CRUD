33a33778192bdbbd9ff4b27f103a7289c829ddbb
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-05-21 13:05:53 +0300

    add CategoryController and display all categories

---
+++ CRUD/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using CRUD.Data;
+using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories.ToList();    
+            return View(categories);
+        }
+    }
+}
